b7c6c7982f009fc115dd0e2c42228f097d94ccf7
MagaliFr <magalifrauendorf@gmail.com>
2023-09-12 09:28:44 +0000

    reduced learning rate

--- ct_classifier/dataset.py
+++ ct_classifier/dataset.py
@@ -108,12 +108,13 @@ class CTDataset(Dataset):
             transforms = T.Compose([
                 T.Pad([pad_amount_x, pad_amount_y]), # pad first
                 T.Resize(self.image_size), # then resize
-                #T.Grayscale(num_output_channels=3),
+                T.Grayscale(num_output_channels=3),
                 T.RandomHorizontalFlip(p=0.5),
                 T.ColorJitter(brightness=.5, hue=.3),
+                T.GaussianBlur(kernel_size=5, sigma=(0.1, 2.5)),
+                T.RandomPerspective(distortion_scale=0.5, p=0.5),
                 T.ToTensor(),
-                T.Normalize(mean=tensor([0.4850, 0.4560, 0.4060]), std=tensor([0.2290, 0.2240, 0.2250])),
-                #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                T.Normalize(mean=tensor([0.4850, 0.4560, 0.4060]), std=tensor([0.2290, 0.2240, 0.2250]))
             ])
         else:
             transforms = T.Compose([
