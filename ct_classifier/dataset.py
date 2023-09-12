7025bc3649a97de05fff798f04944c427ac66ef8
MagaliFr <magalifrauendorf@gmail.com>
2023-09-12 08:28:41 +0000

    normalize val and test as well

--- ct_classifier/dataset.py
+++ ct_classifier/dataset.py
@@ -108,6 +108,7 @@ class CTDataset(Dataset):
             transforms = T.Compose([
                 T.Pad([pad_amount_x, pad_amount_y]), # pad first
                 T.Resize(self.image_size), # then resize
+                #T.Grayscale(num_output_channels=3),
                 T.RandomHorizontalFlip(p=0.5),
                 T.ColorJitter(brightness=.5, hue=.3),
                 T.ToTensor(),
@@ -118,7 +119,8 @@ class CTDataset(Dataset):
             transforms = T.Compose([
                 T.Pad([pad_amount_x, pad_amount_y]), # pad first
                 T.Resize(self.image_size), # then resize
-                T.ToTensor()
+                T.ToTensor(),
+                T.Normalize(mean=tensor([0.4850, 0.4560, 0.4060]), std=tensor([0.2290, 0.2240, 0.2250]))
             ])
         # Apply the transformation with padding
         img_tensor = transforms(img)
